721abf2bd80117ccc0c232b731355664114fe736
Jorge Flores <jflores082413@gmail.com>
2023-01-26 11:33:52 -0800

    final seasonal styles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.getElementById("html-checker").setAttribute("href","https://validator.w
 
 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
 
+
 //manages mobile nav 
 function myFunction() {
     var x = document.getElementById("myTopnav");
